13ca9269e480186ef48c316a380a03f71abd0c85
鸡毛 <shihao.gu@perfma.com>
2018-10-30 13:09:01 +0800

    待实验不同光照

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,6 +2,13 @@
 <!--局部样式-->
 <style scoped>
     .viewIndex {
+        display: inline-block;
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+
+    .threeWarp {
         width: 100%;
         height: 100%;
     }
@@ -14,12 +21,13 @@
 
 <template>
     <div class="viewIndex">
-        <threeCom />
+        <div class="threeWarp"></div>
     </div>
 </template>
 
 <script>
-    import threeCom from "../components/three";
+    import * as THREE from "three";
+    const OrbitControls = require("three-orbit-controls")(THREE);
 
     export default {
         name: "viewIndex",
@@ -29,6 +37,16 @@
         data () {
             return {
                 //#region 页面对象
+                    scene: null,
+                    camera: null,
+                    render: null,
+                    orbitControls: null,
+                    threeWarpDom: null,
+                    $threeWarp: null,
+                    threeSize: {
+                        width: 0,
+                        height: 0,
+                    },
                 //#endregion
 
                 //#region 页面内容绑定数据
@@ -39,7 +57,9 @@
             };
         },
         watch: {
-
+            threeSize (nv) {
+                this.updateThreeSize();
+            },
         },
         computed: {
             //#region 常量计算属性
@@ -69,15 +89,107 @@
 
             //#region 其他方法
             //#endregion
+
+            //#region THREE初始化相关方法
+                //创建摄像机
+                createCamera () {
+                    let camera;
+                    camera = new THREE.PerspectiveCamera(45, this.threeSize.width / this.threeSize.height, 0.1, 1000);
+                    camera.position.x = 0;
+                    camera.position.y = 100;
+                    camera.position.z = 100;
+                    return camera;
+                },
+                //创建渲染器
+                createRender () {
+                    let render;
+                    render = new THREE.WebGLRenderer();
+                    render.setClearColor(0xf0f0f0);
+                    render.setSize(this.threeSize.width, this.threeSize.height);
+                    render.gammaInput = true;
+                    render.gammaOutput = true;
+                    render.shadowMap.enabled = true;
+                    return render;
+                },
+                //初始化轨道控制器
+                initOrbitControls () {
+                    let controls = new OrbitControls(this.camera, this.render.domElement)
+                    controls.enableDamping = true;
+                    controls.dampingFactor = 0.25;
+                    controls.enableZoom = true;
+                    controls.autoRotate = true;
+                    return controls;
+                },
+                //初始化THREE
+                initThree () {
+                    this.threeWarpDom = this.$el.querySelector(".threeWarp");
+                    this.$threeWarp = $(this.threeWarpDom);
+                    this.threeSize.width = this.$threeWarp.width();
+                    this.threeSize.height = this.$threeWarp.height();
+
+                    //创建场景
+                    this.scene = new THREE.Scene();
+                    //创建摄像机
+                    this.camera = this.createCamera();
+                    //创建WebGL渲染器
+                    this.render = this.createRender();
+                    //初始化轨道控制器
+                    this.orbitControls = this.initOrbitControls();
+
+                    this.camera.lookAt(this.scene.position);
+
+                    //添加坐标系
+                    let axes = new THREE.AxesHelper(100);
+                    this.scene.add(axes);
+                    
+                    this.$threeWarp.append(this.render.domElement);
+                    this.render.render(this.scene, this.camera);
+                    this.animateThree();
+                    $(window).resize(this.handleWindowResize);
+                },
+                //更新THREE画布尺寸方法
+                updateThreeSize () {
+                    this.camera.aspect = this.threeSize.width / this.threeSize.height;
+                    this.camera.updateProjectionMatrix();
+                    this.render.setSize(this.threeSize.width, this.threeSize.height);          
+                },
+                //THREE动画驱动
+                animateThree () {
+                    requestAnimationFrame(this.animateThree);
+                    this.render.render(this.scene, this.camera);
+                },
+                //处理Window变化事件
+                handleWindowResize (e) {
+                    let size = {
+                        width: this.$threeWarp.width(),
+                        height: this.$threeWarp.height(),
+                    };
+                    this.threeSize = size;
+                },
+            //#endregion
         },
         created () {
 
         },
         mounted () {
+            this.initThree();
 
+            let light1 = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+            this.scene.add(light1);
+
+            let light2 = new THREE.AmbientLight( 0x404040 ); // soft white light
+            this.scene.add(light2);
+
+            let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+            this.scene.add( directionalLight );
+
+            let geometry = new THREE.BoxGeometry(5, 5, 5);
+            let material = new THREE.MeshLambertMaterial({color: 0x00ff00});
+            let cube = new THREE.Mesh(geometry, material);
+            this.scene.add(cube);
         },
         components: {
-            threeCom,
+
         },
     };
 </script>
