06ad2639a5606dbf7a53cd87e7ef66a91df7693e
鸡毛 <shihao.gu@perfma.com>
2018-10-31 19:42:09 +0800

    思考设计天空盒

--- src/views/index.vue
+++ src/views/index.vue
@@ -99,9 +99,9 @@
                 createCamera () {
                     let camera;
                     camera = new THREE.PerspectiveCamera(45, this.threeSize.width / this.threeSize.height, 0.1, 1000);
-                    camera.position.x = 200;
-                    camera.position.y = 200;
-                    camera.position.z = 200;
+                    camera.position.x = 5;
+                    camera.position.y = 5;
+                    camera.position.z = 5;
                     return camera;
                 },
                 //创建渲染器
@@ -174,6 +174,74 @@
                     this.threeSize = size;
                 },
             //#endregion
+
+            //#region 创建场景对象的方法
+                createGround () {
+                    let planeGeometry = new THREE.PlaneGeometry(22, 22, 32);
+                    let texture = new THREE.TextureLoader().load("/texture/ground.png");
+                    let planeMaterial = new THREE.MeshBasicMaterial({
+                        side: THREE.DoubleSide,
+                        map: texture,
+                    });
+                    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+                    plane.rotateX(Math.PI / 2);
+                    plane.position.set(11, 0, 11);
+                    return plane;
+                },
+
+                createWallBox () {
+                    let geom = new THREE.BoxGeometry(1, 1, 1);
+                    let texture = new THREE.TextureLoader().load("/texture/jinshu.jpeg");
+                    let mate = new THREE.MeshPhongMaterial({
+                        map: texture,
+                    });
+                    let box = new THREE.Mesh(geom, mate);
+                    return box;
+                },
+
+                posTran (x, y) {
+                    return new THREE.Vector3(0.5 + x, 0.5, 0.5 + y);
+                },
+
+                buildWall () {
+                    for (let y = 0; y < 22; ++y) {
+                        for (let x = 0; x < 22; ++x) {
+                            if (y == 0 || x == 0 || y == 21 || x == 21) {
+                                let box = this.createWallBox();
+                                let v = this.posTran(x, y);
+                                box.position.set(v.x, v.y, v.z);
+                                this.scene.add(box);   
+                            }
+                        }
+                    }
+                },
+
+                createSkyBox () {
+                    let geom = new THREE.BoxGeometry(30, 30, 30);
+                    var loader = new THREE.CubeTextureLoader();
+                    loader.setPath("/texture/");
+
+                    var textureCube = loader.load([
+                        '1.png', '2.png',
+                        '3.png', '4.png',
+                        '5.png', '6.png'
+                    ]);
+
+                    var material = new THREE.MeshBasicMaterial( {
+                        color: 0xffffff,
+                        envMap: textureCube,
+                        side: THREE.DoubleSide,
+                    } );
+
+                    let texture = new THREE.TextureLoader().load("/texture/jinshu.jpeg");
+                    // let mate = new THREE.MeshPhongMaterial({
+                    //     map: texture,
+                    // });
+                    let box = new THREE.Mesh(geom, material);
+                    box.position.set(11, 0, 11);
+                    return box;
+                },
+            //#endregion
         },
         created () {
 
@@ -190,62 +258,18 @@
             let directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
             this.scene.add(directionalLight);
 
-            let planeGeometry = new THREE.PlaneGeometry(120, 120, 32);
-            let planeMaterial = new THREE.MeshBasicMaterial({
-                color: "pink",
-                side: THREE.DoubleSide
-            });
-            let plane = new THREE.Mesh( planeGeometry, planeMaterial );
-            plane.rotateX(Math.PI / 2);
-            plane.position.set(60, 0, 60);
+
+            let plane = this.createGround();
             this.scene.add(plane);
 
-            let box1Geom = new THREE.BoxGeometry(120, 10, 10);
-            let box1Mate = new THREE.MeshPhongMaterial({ color: 0x2BAAFF });
-            let box1 = new THREE.Mesh(box1Geom, box1Mate);
-            box1.position.set(60, 5, 5);
-            this.scene.add(box1);
+            // let box = this.createWallBox();
+            // this.scene.add(box);
+            // let v = this.posTran(2, 0);
+            // box.position.set(v.x, v.y, v.z);
+            this.buildWall();
 
-            let box2Geom = new THREE.BoxGeometry(120, 10, 10);
-            let box2Mate = new THREE.MeshPhongMaterial({ color: 0x2BAAFF });
-            let box2 = new THREE.Mesh(box2Geom, box2Mate);
-            box2.position.set(60, 5, 115);
-            this.scene.add(box2);
-
-            let box3Geom = new THREE.BoxGeometry(10, 10, 100);
-            let box3Mate = new THREE.MeshPhongMaterial({ color: 0x2c35FF });
-            let box3 = new THREE.Mesh(box3Geom, box3Mate);
-            box3.position.set(5, 5, 60);
-            this.scene.add(box3);
-
-            let box4Geom = new THREE.BoxGeometry(10, 10, 100);
-            let box4Mate = new THREE.MeshPhongMaterial({ color: 0x2c35FF });
-            let box4 = new THREE.Mesh(box4Geom, box4Mate);
-            box4.position.set(115, 5, 60);
-            this.scene.add(box4);
-
-            for (let y = 0; y < 10; ++y) {
-                for (let x = 0; x < 10; ++x) {
-                    if (Math.random() * 7 < 1) {
-                        let colorList = ["#CDB7B5", "#9BCD9B", "#FF6A6A", "#333333", "#912CEE", "#878787"];
-                        let boxGeom = new THREE.BoxGeometry(10, 10, 10);
-                        let num = Math.floor((Math.random() * 6));
-
-                        var texture = new THREE.TextureLoader().load("/texture/box.png");
-
-                        let boxMate = new THREE.MeshBasicMaterial({
-                            color: 0xffffff,
-                            map: texture,
-                        });
-                        let box = new THREE.Mesh(boxGeom, boxMate);
-                        box.position.set(10 * x + 15, 5, 10 * y + 15);
-                        this.scene.add(box);
-                    }
-                    else {
-                        console.log(2);
-                    }     
-                }
-            }
+            let box = this.createSkyBox();
+            this.scene.add(box);
 
             console.log(THREE);
 
